18c134b07e2bfbc354325fc91b2a4e6852d36809
Lucca <omnichrome@gmail.com>
2021-04-11 22:30:09 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+
+
+// HTTP library
+const http = require('http');
+// filesystem library
+const fs = require('fs');
+
+// port number for HTTP server
+const port = 4242;
+
+// sendFile function - sends files in the HTTP response
+// INPUT:
+//		name of file to be sent
+// 		HTTP response object
+
+const sendFile = function(fileName, res) {
+	// try to read file
+	fs.readFile(fileName, function(error,data) {
+		if (error) {
+			// something went wrong
+			// send a 404 error
+			res.writeHead(404)
+			res.write('Error: File Not Found')
+		} else {
+			// read file successfully
+			// send file
+			res.write(data)
+		}
+		// end HTTP response
+		res.end()
+	})
+}
+
+
+// creating HTTP server
+const server = http.createServer(function(req, res){
+  // files object
+  var routes = {
+    '/':'index.html',
+    '/hello.html':'hello.html',
+    '/sketch.js':'sketch.js',
+    '/client.js':'client.js',
+  }
+  // checking if file is available
+  var hasFile = routes.hasOwnProperty(req.url);
+  if (hasFile) {
+    // file found
+    var fileName = routes[req.url];
+    sendFile(fileName, res);
+  } else {
+    // not found 404
+    res.writeHead(404);
+    res.end();
+  }
+})
+
+// initializing HTTP server
+// listens for requests on the specified port number
+// INPUT: port number
+//
+server.listen(port, function(error) {
+	if (error) {
+		// something went wrong
+		// print error to console
+		console.log('nope', error)
+	} else {
+		// successfully initialized server
+		// print port to console
+		console.log('yep' + port)
+	}
+})
---
+++ sketch.js
@@ -0,0 +1,67 @@
+// canvas properties
+const canvasW = 600; 
+const canvasH = 450;
+
+// thingy properties
+const thingyW = 60;
+const thingyH = 60;
+
+// initial position
+var x = canvasW/2;
+var y = canvasH/2;
+
+// speed
+var speedX = 10;
+var speedY = 10;
+
+// text to be displayed
+var name = 'Lucca';
+
+function setup() {
+  // colors 
+  const colors = ["#F18F01", "#048BA8", "#2E4057", "#99C24D", "#2F2D2E"];
+  const thingy_color = random(colors);
+  
+  // actual canvas setup
+  createCanvas(canvasW, canvasH);
+  
+  // thingy setup
+  rectMode(CENTER);
+  fill(thingy_color);
+}
+
+function draw() {
+  background(220);
+  
+  // handling keypresses
+  if (keyIsDown(LEFT_ARROW)) {
+    x -= speedX;  
+    if (x < thingyW/2) {
+      x = thingyW/2;
+    }
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    x += speedX;  
+    if (x > canvasW-thingyW/2) {
+      x = canvasW-thingyW/2;
+    }
+  }
+  if (keyIsDown(UP_ARROW)) {
+    y -= speedY;  
+    if (y < thingyH/2) {
+      y = thingyH/2;
+    }
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    y += speedY;  
+    if (y > canvasH-thingyH/2) {
+      y = canvasH-thingyH/2;
+    }
+  }
+  
+  // drawing thingy
+  rect(x, y, thingyW, thingyH, 5, 5);
+  
+  // displaying text
+  text(name, x-thingyW/2, y-thingyH/2-8);
+}
